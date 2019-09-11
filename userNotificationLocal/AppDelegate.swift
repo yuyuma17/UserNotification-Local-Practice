b644677f5c366e7165d026f60b4244eeade97349
Lacie <dark830917@gmail.com>
2019-09-11 14:34:05 +0800

    completed. but should add cal and readme

--- userNotificationLocal/AppDelegate.swift
+++ userNotificationLocal/AppDelegate.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            
+            if granted {
+                print("Allowed")
+            }
+            else {
+                print("Not Allowed")
+            }
+        }
+        UNUserNotificationCenter.current().delegate = self
         return true
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .sound, .badge])
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- userNotificationLocal/ViewController.swift
+++ userNotificationLocal/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createNotification()
     }
 
+    func createNotification() {
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = "打我啊笨蛋"
+        content.subtitle = "哈哈"
+        content.body = "還敢下來啊冰鳥"
+        content.badge = 1
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 
 }
 
